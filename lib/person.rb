cd4df20d45596854196a563661a402b6713dfca6
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-08 21:00:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,8 @@
 class Person
 
+  attr_accessor :name, :user_name, :age, :location, :bio
+
+
   def initialize(attributes)
     attributes.each do {|key, value| self.send (("#{key}="), value)}
     end
