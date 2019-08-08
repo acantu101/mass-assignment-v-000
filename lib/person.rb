27dc62a4d7b861cf90d4bcd9cdab143055e36191
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-08 21:50:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-attr_accessor :name, :last_name, :birthday
+attr_accessor :name, :last_name, :birthday, :hair_color
 
    def initialize(attributes)
      attributes.each {|key, value| self.send(("#{key}="), value)}
